af5baeb65453a114118349bbd5f56168c328f5aa
Anthony COLAS <anthonycolas.france@gmail.com>
2020-09-26 00:30:40 +0200

    Different weather cards

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <ResponsiveDrawer /> */}
+      <ResponsiveDrawer />
       <WeatherEngine location="Paris,fr" />
+      <WeatherEngine location="Tokyo,jp" />
+      <WeatherEngine location="San Francisco,us" />
+      <WeatherEngine location="Sydney,au" />
     </div>
   );
 }
